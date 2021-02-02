84388495e52d9b03d01daf0eaf571e2d0ed2b118
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2021-02-02 15:19:25 +0100

    n/a

--- injectLib.c
+++ injectLib.c
@@ -16,10 +16,10 @@ DWORD procNameToId(char* name) {
        printf("CreateToolhelp32Snapshot -> invalid\n");
     }
 
-	  PROCESSENTRY32 ProcEntry = { sizeof(ProcEntry) };
+    PROCESSENTRY32 ProcEntry = { sizeof(ProcEntry) };
 
-	  BOOL MoreMods = Process32First(snap, &ProcEntry);
-	  for (; MoreMods; MoreMods = Process32Next(snap, &ProcEntry)) {
+    BOOL MoreMods = Process32First(snap, &ProcEntry);
+    for (; MoreMods; MoreMods = Process32Next(snap, &ProcEntry)) {
      
 //     printf("%s\n", ProcEntry.szExeFile);
 
@@ -28,13 +28,90 @@ DWORD procNameToId(char* name) {
           return ProcEntry.th32ProcessID; 
        }
 
-//	  	std::tstring CurrentProcess(ProcEntry.szExeFile);
+//      std::tstring CurrentProcess(ProcEntry.szExeFile);
 //          CurrentProcess = toLower(CurrentProcess);
-//	  	Found = (CurrentProcess == Name);
-//	  	if (Found) break;
-	  }
+//      Found = (CurrentProcess == Name);
+//      if (Found) break;
+    }
 
     return 0;
+
+}
+
+
+void injectDll(DWORD procId, char* modulePath) {
+
+     HANDLE process = OpenProcess (
+         PROCESS_QUERY_INFORMATION |   // Required by Alpha
+         PROCESS_CREATE_THREAD     |   // For CreateRemoteThread
+         PROCESS_VM_OPERATION      |   // For VirtualAllocEx/VirtualFreeEx
+         PROCESS_VM_WRITE,             // For WriteProcessMemory
+         FALSE,
+         procId
+     );
+
+     if (!process) {
+        printf("could not open process\n");
+        return;
+     }
+
+//   size_t Size  = (Path.length() + 1) * sizeof(wchar_t);
+     void* mem = VirtualAllocEx(process, NULL, strlen(modulePath+1), MEM_COMMIT, PAGE_READWRITE);
+
+     if (!mem) {
+        printf("could not alloc mem\n");
+        return;
+     }
+
+     if (!WriteProcessMemory(
+        process,
+        mem,
+        modulePath,
+        strlen(modulePath),
+        NULL)
+      ) {
+        printf("could not write process memory.\n");
+        return;
+     }
+
+     HMODULE hKernel32 = GetModuleHandle("Kernel32");
+
+     if (!hKernel32) {
+        printf("! hKernel32.\n");
+     }
+
+     PTHREAD_START_ROUTINE addrLoadLibrary = 
+    (PTHREAD_START_ROUTINE) GetProcAddress(hKernel32, "LoadLibraryW");
+
+
+     if (!addrLoadLibrary) {
+        printf("! addrLoadLibrary\n");
+     }
+
+     HANDLE thread = CreateRemoteThread(
+        process,
+        NULL           , // lpThreadAttributes
+        0              , // dwStackSize
+        addrLoadLibrary, // lpStartAddress
+        mem            , // lpParameter   (mem contains name of library)
+        0              , // dwCreationFlags
+        NULL             // lpThreadId
+     );
+
+	   WaitForSingleObject(thread, INFINITE);
+
+  // Get thread exit code
+      DWORD ExitCode;
+      if (!GetExitCodeThread(thread,&ExitCode)) {
+        printf("! get exit code\n");
+        return;
+      }
+
+  // Check LoadLibrary succeeded and returned a module base
+     if(!ExitCode) {
+        printf("! call to LoadLibraryW failed\n");
+        return;
+     }
 
 }
 
